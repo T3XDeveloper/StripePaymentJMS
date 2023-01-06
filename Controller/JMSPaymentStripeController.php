3002544fd0b4b38e7fdcadad346f7ad8d37daacb
Patrick Wiermann <patrick.wiermann@ommax.de>
2023-01-06 15:25:17 +0100

    add controller testing

--- Controller/JMSPaymentStripeController.php
+++ Controller/JMSPaymentStripeController.php
@@ -4,15 +4,24 @@ namespace JMS\Payment\StripeBundle\Controller;
 
 use Ibexa\Bundle\Core\Controller as BaseController;
 use Symfony\Component\HttpFoundation\Response;
+use Omnipay\Stripe\Gateway;
 
 class JMSPaymentStripeController extends BaseController
 {
+    public function __construct(Gateway $gateway, $apiKey)
+    {
+        $this->gateway = $gateway;
+        $this->apiKey = $apiKey;
+    }
+
     public function requestPaymentmethods(array $payment): Response
     {
         return $this->render(
             __DIR__ . '/../Resources/Rendering/stripe_payment_interface.html.twig',
             [
-                $payment
+                $payment,
+                $this->gateway,
+                $this->apiKey
             ],
         );
     }

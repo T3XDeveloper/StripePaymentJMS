285412b29f78853c87d380048d0daa62099df360
Patrick Wiermann <patrick.wiermann@ommax.de>
2023-01-17 16:54:44 +0100

    adjust payment informations

--- Controller/JMSPaymentStripeController.php
+++ Controller/JMSPaymentStripeController.php
@@ -22,16 +22,28 @@ class JMSPaymentStripeController extends BaseController
         $_SESSION['temp_customer'] = null;
         $_SESSION['temp_intent'] = null;
 
+        $basketID = $payment['basketId'];
+        $basketSessionID = $payment['basketSessionId'];
+
         $stripe = new \Stripe\StripeClient($this->secretKey);
-        $_SESSION['temp_customer'] = $stripe->customers->create([
-            'description' => 'Temped Customer Object for Payment',
+        $existCustomer = $stripe->customers->search([
+            'query' => 'name:\''.$basketID.'&'.$basketSessionID.'\'',
         ]);
+
+        if(!empty($existCustomer->data[0])){
+            $_SESSION['temp_customer'] = $existCustomer->data[0];
+        } else {
+            $_SESSION['temp_customer'] = $stripe->customers->create([
+                'description' => 'DXP Basket-ID: '.$basketID.'; DXP Basket-SessionID: '.$basketSessionID,
+                'name' => $basketID.'&'.$basketSessionID
+            ]);
+        }
 
         $_SESSION['temp_intent'] = $stripe->paymentIntents->create(
             [
                 'customer' => $_SESSION['temp_customer']->id,
                 'setup_future_usage' => 'on_session',
-                'amount' => intval($payment['amount']),
+                'amount' => $payment['amount'] * 100,
                 'currency' => $payment['currency'],
                 'automatic_payment_methods' => ['enabled' => true],
             ]

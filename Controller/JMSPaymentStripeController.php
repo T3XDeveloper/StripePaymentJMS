7a49564917ead5e3a794f39382210c1badcf0ef7
Patrick Wiermann <patrick.wiermann@ommax.de>
2023-03-01 12:51:41 +0100

    set fix payments

--- Controller/JMSPaymentStripeController.php
+++ Controller/JMSPaymentStripeController.php
@@ -52,8 +52,7 @@ class JMSPaymentStripeController extends BaseController
                 'setup_future_usage' => 'on_session',
                 'amount' => round($payment['amount'], 2) * 100,
                 'currency' => $payment['currency'],
-                'automatic_payment_methods' => ['enabled' => false],
-                'payment_method_types' => ['card', 'klarna']
+                'automatic_payment_methods' => ['enabled' => true],
             ]
         );
 
